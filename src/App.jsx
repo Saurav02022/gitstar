845e662a8e750cecbfe16a8b3a1dcd02f7a49065
SAURAV KUMAR <sk729584@gmail.com>
2022-12-11 13:01:01 +0530

    Delete Route file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import All from "./Components/All";
+import Html from "./Components/Html";
+import Css from "./Components/Css";
+import JavaScript from "./Components/JavaScript";
 import Navbar from "./Components/Navbar";
-import Router from "./Router";
 function App() {
   return (
     <div>
-<Router/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<All />} />
+        <Route path="/html" element={<Html />} />
+        <Route path="/css" element={<Css />} />
+        <Route path="/javascript" element={<JavaScript />} />
+      </Routes>
     </div>
   );
 }
